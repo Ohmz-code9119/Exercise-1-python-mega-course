0856ab16fafe3b91f3f6453f3b1ded8013f4d766
omarm <omarmourad9119@gmail.com>
2023-08-30 00:39:21 -0400

    Exercise 1 part 2: added the text in the text files to its own respective PDF page

--- main.py
+++ main.py
@@ -14,8 +14,14 @@ for filepath in filepaths:
     pdf.add_page()
     filename = Path(filepath).stem
     name = filename.title()
-    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=name, ln=1)
+
+    with open(filepath, "r") as text:
+        content = text.read()
+
+    pdf.set_font(family="Times", size=8)
+    pdf.multi_cell(w=0, h=4, txt=content)
 
 pdf.output("output.pdf")
 
